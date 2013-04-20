f6ff90fb6bb9d536d9b8dd37b1e795776f51e5dd
fubukiefsf <fubuki.efsf11@gmail.com>
2013-04-21 02:24:43 +0900

    ショートカットキーを設定することでPOINTの確認と所持ポイントをつぶやくことができるようになった。 所持ポイントが3500POINT以下の時"#ふぶき氏ポイント"がTLに流れてくると全ポイント旅立つ

--- fubukiefsf_point.rb
+++ fubukiefsf_point.rb
@@ -2,45 +2,69 @@
 
 Plugin.create(:fubukiefsf_point) do
 	launched_time = Time.now
+	UserConfig[:fubukiefsf_point_to_system] ||= false
 	UserConfig[:fubukishiPoint] ||= 10000
 	UserConfig[:fubukishiTripPoint] ||= 3500
 	UserConfig[:getFubukishiPoint] ||= 10000
 	UserConfig[:enkunUnfavPoint] ||= 500
-
+	#ms = "";
 	
 	on_appear do |mess|
 		mess.each do |mes|
 			if mes.message.to_s.include?("#ふぶき氏ポイント")
 				if UserConfig[:fubukishiPoint] <= 0
-					operateFubukishiPoint(0,"")
-				#elsif
+					ms = operateFubukishiPoint(0,"")
  				else
-					operateFubukishiPoint(UserConfig[:fubukishiPoint],"use")
+					ms = operateFubukishiPoint(UserConfig[:fubukishiPoint],"use")
 				end
+				messageFubukishiPoint(ms)
 			end
 		end
 	end
 	on_unfavorite do |service,user,message|
 		if message.user.id == 159733526
-			gainFubukishiPoint("enkun")
+			ms = gainFubukishiPoint("enkun")
 		end
+		messageFubukishiPoint(ms)
 	end
 	
 	on_favorite do |service,user,message|
-		if message.user.id == 51428532 
-			gainFubukishiPoint("fubukishi_fav")
-		elsif message.user.id == 15926668
-			gainFubukishiPoint("toshi_a_fav")
+		if message.user.is_me?	
+			if message.user.id == 51428532 
+				ms = gainFubukishiPoint("fubukishi_fav")
+			elsif message.user.id == 15926668
+				ms = gainFubukishiPoint("toshi_a_fav")
+			end
 		end
+		messageFubukishiPoint(ms)
+	end
+
+	#if(!ms)
+	#end
+	command(
+		:fubukiefsf_point,
+		name: "ふぶき氏ポイントを確認する",
+		visible: false,
+		condition: lambda{|opt| true},
+		role: :window
+	)do |opt|
+		messageFubukishiPoint("You have #{UserConfig[:fubukishiPoint]} fubuki-shi points! Enjoy!")			
+		Post.primary_service.update(:message => "ふぶき氏ポイントを#{UserConfig[:fubukishiPoint]}point持ってるよ" ,:system => UserConfig[:fubukiefsf_point_to_systemt] )
+	end
+	
+	def repairFubukishiPoint
+		Reserver.new("0:00"){
+			UserConfig[:fubukishiPoint] += UserConfig[:getFubukishiPoint]
+		}
 	end
 
 	def gainFubukishiPoint(mode)
 		if mode == "enkun"
-			self.operateFubukishiPoint(1000,"gain")
+			return self.operateFubukishiPoint(1000,"gain")
 		elsif mode == "fubukishi_fav"
-			self.operateFubukishiPoint(200,"gain")
+			return self.operateFubukishiPoint(200,"gain")
 		elsif mode == "toshi_a_fav"
-			self.operateFubukishiPoint(500,"gain")	
+			return self.operateFubukishiPoint(500,"gain")	
 		end
 	end
 	 
@@ -49,16 +73,20 @@ Plugin.create(:fubukiefsf_point) do
 			UserConfig[:fubukishiPoint] += point
 		elsif action == "use"
 			if UserConfig[:fubukishiPoint] < UserConfig[:fubukishiTripPoint]
-				activity :system, "ポイントが足りないよ"
-				return
+				UserConfig[:fubukishiPoint] -= UserConfig[:fubukishiPoint]
+				return "全ポイント旅立ったよ"
 			else
 				UserConfig[:fubukishiPoint] -= point
 			end
 		else
-			activity :system, " Too Late..."
-			return
+			return " Too Late..."
 		end	
-		activity :system, " #{action} #{point} points fubuki-shi point."
+		return " #{action} #{point} points fubuki-shi point."
 	end
+	
+	def messageFubukishiPoint(mes)
+		activity :system, mes
+	end 
+	
 end
 
